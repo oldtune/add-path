f91c580857be69d5b25e37731f8d7f528477644a
Do Tran <do.tran@saigontechnology.com>
2021-11-21 22:27:35 +0700

    first commit

---
+++ AddEnvironmentPath/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("Path required, please use it like this: concatpath {your_path_here}");
+            }
+
+            var pathToAdd = args[0];
+
+            var currentPathValue = GetEnvironmentVariable("Path");
+            
+            var newPath = currentPathValue != null 
+                ? $"{currentPathValue};{pathToAdd}"
+                : pathToAdd;
+
+            Environment.SetEnvironmentVariable("Path", newPath, EnvironmentVariableTarget.User);
+        }
+
+        public static string? GetEnvironmentVariable(string name)
+        {
+            var value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User);
+            return value;
+        }
+    }
+}
